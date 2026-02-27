d648dfd7669b63d0271511c8a8fe77b92997da42
Sanchitosss <Sashashurnov.777@gmail.com>
2026-02-27 23:57:07 +0300

    Update двусвязный список.py

--- двусвязный массив.py
+++ двусвязный массив.py
@@ -1,48 +1,59 @@
+# Узел
 class Node:
     def __init__(self, name):
         self.name = name
         self.next = None
 
-
+# Список
 class ListNode:
     def __init__(self):
+        # Голова списка
         self.head = None
 
+    # Добавление нового элемента в конец
     def add(self):
         name = input('Введите имя: ')
         current = self.head
 
+        # Если список пустой
         if not current:
             self.head = Node(name)
             return
 
+        # Перебираем список до последнего узла
         while current.next:
             current = current.next
         current.next = Node(name)
 
+    # Просмотр всех элементов
     def view(self):
         current = self.head
 
+        # Если список пустой
         if not current:
             print('Список пуст!')
             return
 
+        # выводим все узлы
         while current:
             print(current.name)
             current = current.next
 
+    # Удаление последнего элемента
     def delete(self):
-
         current = self.head
 
+        # Если список пустой
         if not current:
             print('Список пуст!')
             return
 
+        # Если только один элемент
         elif not current.next:
             self.head = None
             return
 
+        # Перебираем до предпоследнего
         while current.next.next:
             current = current.next
         current.next = None
@@ -68,3 +79,4 @@ def menu():
 
 
 menu()
+
